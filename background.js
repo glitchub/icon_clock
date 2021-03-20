3ed4ff41cb9f55b82449098632345183970aa7c4
glitchub <glitchub@users.noreply.github.com>
2021-03-20 09:22:43 -0700

    Version 6: fix bounding box for Windows font, etc

--- background.js
+++ background.js
@@ -1,38 +1,39 @@
 // Icon Clock
 
-// default params, overriden by storage
-var color = "#808080";
-var hours = 24;
+var color;
+var hours;
 
-// const font = "bold 256px sans-serif";
+// Use a large font since it will be scaled down
 // XXX select font family and weight as an option
-const font = "256px sans-serif"; // make it large since it will be scaled down
+// const font = "bold 256px sans-serif";
+const font = "256px sans-serif";
 
 {
-    // Determine the exact bounding box for 2x2 digits
+    // Determine the exact bounding box for 2-bt-2 digits at 256px
     let can = document.createElement("canvas");
     let con = can.getContext("2d");
     con.font = font;
     con.textBaseline = "top";
 
-    // look for worst case of each
+    // Get worst case metrics for all digits.
     let mleft = 999, mright = 0, masc = 999, mdesc = 0;
     pairs = ["11", "22", "33", "44", "55", "66", "77", "88", "99", "00"];
     pairs.forEach((p) => {
         let m = con.measureText(p);
-        if (m.actualBoundingBoxLeft < mleft) mleft = m.actualBoundingBoxLeft;
-        if (m.actualBoundingBoxRight > mright) mright = m.actualBoundingBoxRight;
-        if (-m.actualBoundingBoxAscent < masc) masc = -m.actualBoundingBoxAscent; // negative!
-        if (m.actualBoundingBoxDescent > mdesc) mdesc = m.actualBoundingBoxDescent;
+        // Some fonts return negative metrics? abs everything.
+        mleft = Math.min(mleft, Math.abs(m.actualBoundingBoxLeft));    // Leftest,
+        masc = Math.min(masc, Math.abs(m.actualBoundingBoxAscent));    // highest,
+        mright = Math.max(mright, Math.abs(m.actualBoundingBoxRight)); // rightest,
+        mdesc = Math.max(mdesc, Math.abs(m.actualBoundingBoxDescent)); // and lowest.
     });
     // console.log("mleft="+mleft, "mright="+mright, "masc="+masc, "mdesc="+mdesc);
 
-    let gap = mdesc * .2;                           // gap between lines
+    let gap = mdesc * 0.18;                         // gap between lines
     var bleft = mleft;                              // bounded left offset
     var btop = masc;                                // bounded top offset
     var bwidth = mright - mleft;                    // bounded width
     var bheight = (mdesc * 2) + gap - masc;         // bounded height
-    // console.log("second="+bsecond, "bleft="+bleft, "btop="+btop, "bwidth="+bwidth, "bheight"+bheight);
+    // console.log("bleft="+bleft, "btop="+btop, "bwidth="+bwidth, "bheight"+bheight);
 
     var dsecond = mdesc + gap;                      // draw offset to top of second line
     var dwidth = mright + mleft;                    // draw width
@@ -55,7 +56,6 @@ function update()
 
     let date = new Date();
 
-    // maybe adjust  12h
     let hh = date.getHours();
     if (hours == 12)
     {
@@ -63,37 +63,37 @@ function update()
         else if (hh > 12) hh -= 12;
     }
 
-    // hours on top line (with leading "0")
+    // Put hours on top line, with leading "0"
     con.fillText(("0" + hh).slice(-2), 0, 0);
 
-    // minutes on the bottom line
+    // Put minutes on the bottom line, with leading "0"
     con.fillText(("0" + date.getMinutes()).slice(-2), 0, dsecond);
 
-    // update icon with bounded text (it will be scaled to fit)
+    // Update icon with bounded text (it will be scaled to fit)
     browser.browserAction.setIcon({imageData: con.getImageData(bleft, btop, bwidth, bheight)});
 
-    // also show date on hover
+    // Also show date on hover
     browser.browserAction.setTitle({title: date.toDateString()});
 
-    // schedule timer for another update 100mS after the next minute
+    // Schedule another update 100mS after the next minute
     clearTimeout(timer);
     timer = setTimeout(update, 60100 - (Date.now() % 60000));
 }
 
 // Set hours and color from storage and do the first update
-browser.storage.local.get().then((res) =>
+browser.storage.local.get().then((res)=>
 {
     // console.log("Got local storage", res.hours, res.color);
-    if (res.color) color = res.color;
-    if (res.hours) hours = res.hours;
+    color = res.color || "#ffff00";
+    hours = res.hours || 24;
     update();
 });
 
 // Handle storage changes from options.js
-browser.storage.onChanged.addListener((change, name) =>
+browser.storage.onChanged.addListener((change, name)=>
 {
     // console.log("Change %o %s", change, name);
-    if (name == "local")
+    if (name == "local" && (change.hours || change.color))
     {
         if (change.hours) hours = change.hours.newValue;
         if (change.color) color = change.color.newValue;
@@ -102,4 +102,4 @@ browser.storage.onChanged.addListener((change, name) =>
 });
 
 // Open options if the icon is clicked
-browser.browserAction.onClicked.addListener(() => browser.runtime.openOptionsPage());
+browser.browserAction.onClicked.addListener(()=>browser.runtime.openOptionsPage());
--- options.js
+++ options.js
@@ -1,44 +1,37 @@
 // Icon Clock options
 
-// Note inline scripts are disallowed so we use event listeners.
+// Note inline scripts are disallowed, use event listeners.
 
-var color = "#808080"
-var hours = 0;
+var color;
+var hours;
 
 // Initialize on load.
-function init()
-{
+document.addEventListener("DOMContentLoaded", ()=>{
     browser.storage.local.get().then((s)=>{
-        if (s.color) color = s.color;
-        if (s.hours) hours = s.hours;
+        color = s.color || "#ffff00";
+        hours = s.hours || 24;
+        document.getElementById("hours").value = hours;
     });
-}
-document.addEventListener("DOMContentLoaded", init);
+});
 
-// Set 12 or 24 hours when corresponding button is clicked.
-function uphours(h)
-{
+// Set 12 or 24 hours when the selection changes
+document.getElementById("hours").addEventListener("change", ()=>{
+    let h = document.getElementById("hours").value;
     if (hours != h) { hours = h; browser.storage.local.set({"hours" : hours}); }
-}
-document.getElementById("hour12").addEventListener("click", () => { uphours(12); });
-document.getElementById("hour24").addEventListener("click", () => { uphours(24); });
+});
 
 // Popup the color input dialog when the color button is clicked (the real color
 // button is hidden).
-function popup()
-{
-    let p = document.getElementById("popup");
+document.getElementById("popup").addEventListener("click", ()=>{
+    let p = document.getElementById("color");
     p.focus();
     p.value = color;
     p.click();
-}
-document.getElementById("color").addEventListener("click", popup);
+});
 
 // Update color in real time as it changes (background.js will receive the
 // storage events and redraw the clock).
-function upcolor()
-{
-    let v = document.getElementById("popup").value;
-    if (color != v) { color = v; browser.storage.local.set({"color" : color}); }
-}
-document.getElementById("popup").addEventListener("input", upcolor);
+document.getElementById("color").addEventListener("input", ()=>{
+    let c = document.getElementById("color").value;
+    if (color != c) { color = c; browser.storage.local.set({"color" : color}); }
+});
